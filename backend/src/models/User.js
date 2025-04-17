4f117f213e8fa82b480189e496339f6efd1a516d
naseer-bhat <studymail2k18@gmailo.com>
2025-04-17 23:19:09 +0530

    added folder structure for backend

---
+++ backend/src/models/User.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true, maxlength: 30 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, unique: true, minlength: 8 },
+  phone: { type: String, required: true, unique: true },
+  role: {
+    type: String,
+    enum: ["admin", "coordinator", "participant"],
+    default: "participant",
+  },
+});
+export const User = model("User", UserSchema);
