fb1031bc25984e97edd27c945bd3209be54d743e
naseer-bhat <studymail2k18@gmail.com>
2025-06-20 16:10:34 +0530

    added welcomeRoute

--- backend/src/routes/welcomeRoute.js
+++ backend/src/routes/welcomeRoute.js
@@ -1,6 +1,6 @@
 import{Router} from  "express"
 const router=Router()
-router.get("/welcome",async ()=>{
+router.get("/welcome",async (req,res)=>{
   res.send("welcome to my project")
 })
 export default router
