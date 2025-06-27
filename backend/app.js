9243714eefae9b6ffe98454543707ad3e9d0cbc8
naseer-bhat <studymail2k18@gmail.com>
2025-06-27 11:11:56 +0530

    added Welcome Route

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { connectDB } from "./src/config/db.js";
 import allRoutes from "./src/routes/index.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const dbUrl = process.env.DBURI;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 connectDB(dbUrl);
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'welcome.html'))
+})
+app.get('/')
 app.use("/api", allRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- backend/src/middlewares/authenticateToken.js
+++ backend/src/middlewares/authenticateToken.js
@@ -2,9 +2,9 @@ import jwt from "jsonwebtoken";
 const authenticateToken = (req, res, next) => { 
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401); // Unauthorized
+  if (!token) return res.sendStatus(401); 
   jwt.verify(token,process.env.JWTSECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // Forbidden
+    if (err) return res.sendStatus(403); 
     req.user = user;
     next();
   });     
